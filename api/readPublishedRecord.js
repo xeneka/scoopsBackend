1edaca57d3732de7abd9d8c31d0a7b235aacf62d
Antonio Benavente del Moral <antonio@benavente-cardador.com>
2016-10-30 17:05:56 +0100

    APi lector de registros para el usuario

--- api/readPublishedRecord.js
+++ api/readPublishedRecord.js
@@ -17,7 +17,7 @@ var api =  {
 
         var query = {
 
-            sql: "select * from  Autors where publicada=1 order by updateAt DESC",
+            sql: "select * from  Autors where publicada=1 order by updatedAt DESC",
 
 
         req.azureMobile.data.execute(query).then(function(result){
