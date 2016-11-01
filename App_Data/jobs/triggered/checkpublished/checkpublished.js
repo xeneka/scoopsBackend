5076b44a989d8dbfea246fce8c90b5d28f52d704
Antonio Benavente del Moral <antonio@benavente-cardador.com>
2016-11-01 07:40:06 +0100

    Webjob

--- App_Data/jobs/triggered/checkpublished/checkpublished.js
+++ App_Data/jobs/triggered/checkpublished/checkpublished.js
@@ -3,4 +3,18 @@
  */
 'use strict'
 
-console.log("HOLA");
+var sql = require('mssql');
+
+sql.connect("mssql://{adminscoop}:{-ana2008}@{misqlserver}.database.windows.net:1433/{mibasededatos}?encrypt=true")
+    .then(function() {
+
+        new sql.Request().query("UPDATE news SET published = 1")
+            .then(function(recordset) {
+                console.log("All news published");
+            }).catch(function(err) {
+            console.error(err);
+        });
+
+    }).catch(function(err) {
+    console.error(err);
+});
